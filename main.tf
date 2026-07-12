dc65ce0b40546b3fce23419b0ca1872792f4e9a0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:44 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,13 +1,23 @@
+data "azurerm_key_vault_secret" "account_key" {
+  for_each     = { for k, v in var.machine_learning_datastore_fileshares : k => v if v.account_key_key_vault_id != null && v.account_key_key_vault_secret_name != null }
+  name         = each.value.account_key_key_vault_secret_name
+  key_vault_id = each.value.account_key_key_vault_id
+}
+data "azurerm_key_vault_secret" "shared_access_signature" {
+  for_each     = { for k, v in var.machine_learning_datastore_fileshares : k => v if v.shared_access_signature_key_vault_id != null && v.shared_access_signature_key_vault_secret_name != null }
+  name         = each.value.shared_access_signature_key_vault_secret_name
+  key_vault_id = each.value.shared_access_signature_key_vault_id
+}
 resource "azurerm_machine_learning_datastore_fileshare" "machine_learning_datastore_fileshares" {
   for_each = var.machine_learning_datastore_fileshares
 
   name                    = each.value.name
   storage_fileshare_id    = each.value.storage_fileshare_id
   workspace_id            = each.value.workspace_id
-  account_key             = each.value.account_key
+  account_key             = each.value.account_key != null ? each.value.account_key : try(data.azurerm_key_vault_secret.account_key[each.key].value, null)
   description             = each.value.description
   service_data_identity   = each.value.service_data_identity
-  shared_access_signature = each.value.shared_access_signature
+  shared_access_signature = each.value.shared_access_signature != null ? each.value.shared_access_signature : try(data.azurerm_key_vault_secret.shared_access_signature[each.key].value, null)
   tags                    = each.value.tags
 }
 
--- variables.tf
+++ variables.tf
@@ -7,21 +7,29 @@ Required:
     - workspace_id
 Optional:
     - account_key
+    - account_key_key_vault_id (alternative to account_key - read from Key Vault instead)
+    - account_key_key_vault_secret_name (alternative to account_key - read from Key Vault instead)
     - description
     - service_data_identity
     - shared_access_signature
+    - shared_access_signature_key_vault_id (alternative to shared_access_signature - read from Key Vault instead)
+    - shared_access_signature_key_vault_secret_name (alternative to shared_access_signature - read from Key Vault instead)
     - tags
 EOT
 
   type = map(object({
-    name                    = string
-    storage_fileshare_id    = string
-    workspace_id            = string
-    account_key             = optional(string)
-    description             = optional(string)
-    service_data_identity   = optional(string) # Default: "None"
-    shared_access_signature = optional(string)
-    tags                    = optional(map(string))
+    name                                          = string
+    storage_fileshare_id                          = string
+    workspace_id                                  = string
+    account_key                                   = optional(string)
+    account_key_key_vault_id                      = optional(string)
+    account_key_key_vault_secret_name             = optional(string)
+    description                                   = optional(string)
+    service_data_identity                         = optional(string) # Default: "None"
+    shared_access_signature                       = optional(string)
+    shared_access_signature_key_vault_id          = optional(string)
+    shared_access_signature_key_vault_secret_name = optional(string)
+    tags                                          = optional(map(string))
   }))
 }
 
